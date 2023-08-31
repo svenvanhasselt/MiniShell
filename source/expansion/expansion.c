f30573e716757a7902e2cdaa34fb89835e082f1b
svennington <svenvanhasselt@gmail.com>
2023-08-31 16:41:13 +0200

    norm fixes

--- source/expansion/expansion.c
+++ source/expansion/expansion.c
@@ -6,64 +6,45 @@
 /*   By: sven <sven@student.42.fr>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/01 18:26:09 by svan-has      #+#    #+#                 */
-/*   Updated: 2023/08/31 16:17:20 by svan-has      ########   odam.nl         */
+/*   Updated: 2023/08/31 16:39:42 by svan-has      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-char	*find_variable(char *variable, enum e_token type, char ***env)
+void	expand(t_node *head, t_node	**node)
 {
-	int		var_set;
-	char	*value;
+	if (ft_strnstr(head->str, " ", ft_strlen(head->str)))
+		*node = split_variable(head);
+}
 
-	var_set = find_env_var(variable, (*env));
-	if (var_set >= 0)
-	{
-		value = null_check(ft_strdup(((*env)[var_set] + find_value((*env)[var_set]) + 1)));
-		free(variable);
-		return (value);
-	}
-	if (!ft_strncmp(variable, "$", ft_strlen(variable)))
-		return (variable);
-	else if (type == EXP)
-		return (NULL);
-	return (NULL);
+void	expand_exit_code(t_node *head, int exit_status)
+{
+	free(head->str);
+	head->str = null_check((ft_itoa(exit_status)));
 }
 
 void	expand_variable(t_node **lst, char ***env, int exit_status)
 {	
 	t_node	*head;
 	t_node	*prev;
-	t_node	*current;
 
 	head = *lst;
 	prev = head;
 	while (head)
 	{
-		current = head;
 		if (head->type == ENV)
 		{
 			if (!ft_strncmp(head->str, "?", ft_strlen(head->str)))
 			{
-				free(head->str);
-				head->str = null_check((ft_itoa(exit_status)));
+				expand_exit_code(head, exit_status);
+				continue;
 			}
-			else if (*lst == head)
-			{
-				head->str = find_variable(head->str, head->type, env);
-				if (ft_strnstr(head->str, " ", ft_strlen(head->str)))
-				{
-					*lst = split_variable(head);
-					// free(current);
-				}
-			}
+			head->str = find_variable(head->str, head->type, env);
+			if (*lst == head)
+				expand(head, lst);
 			else
-			{
-				head->str = find_variable(head->str, head->type, env);
-				if (ft_strnstr(head->str, " ", ft_strlen(head->str)))
-					prev = split_variable(head);
-			}
+				expand(head, &prev);
 		}
 		prev = head;
 		head = head->next;
--- source/expansion/utils.c
+++ source/expansion/utils.c
@@ -1,57 +1,32 @@
-// /* ************************************************************************** */
-// /*                                                                            */
-// /*                                                        ::::::::            */
-// /*   utils.c                                            :+:    :+:            */
-// /*                                                     +:+                    */
-// /*   By: sven <sven@student.42.fr>                    +#+                     */
-// /*                                                   +#+                      */
-// /*   Created: 2023/08/02 12:36:31 by svan-has      #+#    #+#                 */
-// /*   Updated: 2023/08/16 17:32:54 by svan-has      ########   odam.nl         */
-// /*                                                                            */
-// /* ************************************************************************** */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   utils.c                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: svan-has <svan-has@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/08/31 16:35:32 by svan-has      #+#    #+#                 */
+/*   Updated: 2023/08/31 16:35:35 by svan-has      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
 
-// #include <minishell.h>
+#include <minishell.h>
 
-// char	*find_variable(char *variable, enum e_token type, char ***env)
-// {
-// 	int		var_set;
-// 	char	*value;
+char	*find_variable(char *variable, enum e_token type, char ***env)
+{
+	int		var_set;
+	char	*value;
 
-// 	printf("var: %s\n", variable);
-// 	var_set = find_env_var(variable, (*env));
-// 	if (var_set >= 0)
-// 	{
-// 		value = null_check(ft_strdup(((*env)[var_set] + find_value((*env)[var_set]) + 1)));
-// 		free(variable);
-// 		return (value);
-// 	}
-// 	if (!ft_strncmp(variable, "$", ft_strlen(variable)))
-// 		return (variable);
-// 	else if (type == EXP)
-// 		return (NULL);
-// 	return (NULL);
-// }
-
-// t_node	*split_variable(t_node *lst)
-// {
-// 	int		i;
-// 	char	**split_str;
-// 	t_node	*word_split;
-// 	t_node	*node;
-
-
-// 	word_split = NULL;
-// 	split_str = null_check(ft_split(lst->str, ' '));
-// 	free(lst->str);
-// 	i = 0;
-// 	while (split_str[i])
-// 	{
-// 		node = make_node(split_str[i], ft_strlen(split_str[i]), lst->type, EXP);
-// 		ft_add_back_list(&word_split, node);
-// 		i++;
-// 	}
-// 	node = ft_lastlist(word_split);
-// 	node->next = lst->next;
-// 	free(lst);
-// 	return (word_split);
-// }
+	var_set = find_env_var(variable, (*env));
+	if (var_set >= 0)
+	{
+		value = null_check(ft_strdup(((*env)[var_set] + find_value((*env)[var_set]) + 1)));
+		free(variable);
+		return (value);
+	}
+	if (!ft_strncmp(variable, "$", ft_strlen(variable)))
+		return (variable);
+	else if (type == EXP)
+		return (NULL);
+	return (NULL);
+}
