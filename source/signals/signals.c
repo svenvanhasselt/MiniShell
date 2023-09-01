082fa3e19c261c3bce2760359460e8fc2afc6506
svennington <svenvanhasselt@gmail.com>
2023-09-01 18:24:20 +0200

    test

--- source/signals/signals.c
+++ source/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: sven <sven@student.42.fr>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/03 16:19:50 by svan-has      #+#    #+#                 */
-/*   Updated: 2023/09/01 18:15:56 by svan-has      ########   odam.nl         */
+/*   Updated: 2023/09/01 18:24:10 by svan-has      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ void	signal_action_C_parent()
 	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
+	printf("test\n");
 }
 
 void	signal_action_quit_child()
